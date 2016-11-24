cf2245658ee20f82e66250836af493792cb31338
César Henrique Bernabé <cesar.hber@gmail.com>
2016-11-23 22:58:40 -0200

    Leitura de todas as entradas

--- sudoku.py
+++ sudoku.py
@@ -15,71 +15,74 @@ from coloracaolargura import ColoracaoLargura
 def main(argv):
 
 	# #chama algoritmo
-	a = WelshPowell()
-	# a = ColoracaoLargura()
-	tempos = 0
-	falhas = []
-	exatos = 0
+	# a = WelshPowell()
+	a = ColoracaoLargura()
 
-	for i in range(10):
-		# open and read file
-		fp = open("entrada5.txt", 'r')
-		data = fp.read().split('\n')
-		data.remove('')
+	for i in range(2, 6):
+		tempos = 0
+		falhas = []
+		exatos = 0
 
-		#pega tamanho do sudoku
-		n = int(data[0])
-		del data[0]
+		entrada = "entrada%d.txt" % (i)
+		for i in range(10):
+			# open and read file
+			fp = open(entrada, 'r')
+			data = fp.read().split('\n')
+			data.remove('')
 
-		#inicializa
-		g = Grafo(n)
-		g.montar()
+			#pega tamanho do sudoku
+			n = int(data[0])
+			del data[0]
 
-		#le elementos estaticos
-		for line in data:
-			verticeEstatico = line.split(';')
-			i = int(verticeEstatico[0])
-			j = int(verticeEstatico[1])
-			color = int(verticeEstatico[2])
-			g.colorir(i, j, color)
+			#inicializa
+			g = Grafo(n)
+			g.montar()
 
-		#transforma de matriz para lista
-		g.vertices = g.verticesArray()
+			#le elementos estaticos
+			for line in data:
+				verticeEstatico = line.split(';')
+				i = int(verticeEstatico[0])
+				j = int(verticeEstatico[1])
+				color = int(verticeEstatico[2])
+				g.colorir(i, j, color)
 
-		#roda o algoritmo
-		inicio = time.clock()
-		# a.cl(g)
-		a.wp(g)
-		fim = time.clock()
+			#transforma de matriz para lista
+			g.vertices = g.verticesArray()
 
-		tempos += fim-inicio
+			#roda o algoritmo
+			inicio = time.clock()
+			a.cl(g)
+			# a.wp(g)
+			fim = time.clock()
 
-		if g.falhas == 0:
-			exatos += 1
+			tempos += fim-inicio
+
+			if g.falhas == 0:
+				exatos += 1
+			else:
+				falhas.append(g.falhas)
+
+		#calcula resultados
+		media = 0
+		for f in falhas:
+			media += f
+		if len(falhas):
+			media /= len(falhas)
 		else:
-			falhas.append(g.falhas)
+			falhas.append(0)
+		falhas.sort()
 
-	#calcula resultados
-	media = 0
-	for f in falhas:
-		media += f
-	if len(falhas):
-		media /= len(falhas)
-	else:
-		falhas.append(0)
-	falhas.sort()
+		#imprime resultados
+		print "--------------- RESULTADOS --------------"
+		print "Tempo Médio", tempos/10
+		print "Restrições violadas:"
+		print "\tMédia:\t", media
+		print "\tMelhor:\t", falhas[0]
+		print "\tPior:\t", falhas[-1]
+		print "Soluções corretas:", exatos
 
-	#imprime resultados
-	print "--------------- RESULTADOS --------------"
-	print "Tempo Médio", tempos/10
-	print "Restrições violadas:"
-	print "\tMédia:\t", media
-	print "\tMelhor:\t", falhas[0]
-	print "\tPior:\t", falhas[-1]
-	print "Soluções corretas:", exatos
-
-	# #imprime resultado
-	# g.imprimir()
+		# #imprime resultado
+		# g.imprimir()
 
 if __name__ == '__main__':
 	main(sys.argv)
